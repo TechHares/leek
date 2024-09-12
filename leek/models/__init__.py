b16cf33bf6bde7d5a3caa5ff7b25bf56ad7409bf
lishenglin <shenglin.li@metalpha.finance>
2024-09-12 12:47:44 +0800

    feat(code): migrations
    1. 上传数据库变更

---
+++ leek/models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/9/11 14:13
+# @Author  : shenglin.li
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+if __name__ == '__main__':
+    pass
---
+++ leek/models/xg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/9/11 20:14
+# @Author  : shenglin.li
+# @File    : xg.py
+# @Software: PyCharm
+
+if __name__ == '__main__':
+    pass

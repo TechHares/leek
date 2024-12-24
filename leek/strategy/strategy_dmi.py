f867bacf86bee7202d7d302ad104e3057db41106
lishenglin <shenglin.li@metalpha.finance>
2024-12-24 20:56:35 +0800

    feat(code,doc): 新指标
    - 增加指标
    - 增加DMI策略

--- leek/strategy/strategy_dmi.py
+++ leek/strategy/strategy_dmi.py
@@ -14,7 +14,7 @@ from leek.t import DMI, StochRSI
 from leek.trade.trade import PositionSide
 
 
-class DMIStrategy(DynamicRiskControl, JustFinishKData, BaseStrategy):
+class DMIStrategy(DynamicRiskControl, JustFinishKData, PositionRateManager, BaseStrategy):
     verbose_name = "DMI择时例子"
 
     def __init__(self):
@@ -75,10 +75,9 @@ class DMIStrategy(DynamicRiskControl, JustFinishKData, BaseStrategy):
         if self.market_data.finish != 1:
             adx_last.append(k.adx)
         adx_cross = self.g.pre_adx < self.adx_threshold < k.adx
+        logger.debug(f"DMI,{adx_cross} finish:{self.market_data.finish == 1}, close:{k.close}, stop_loss_price:{self.g.stop_loss_price}"
+                    f"pdi:{k.up_di} {self.g.pre_up_di}, mdi:{k.down_di} {self.g.pre_down_di}, adx:{k.adx} {self.g.pre_adx}")
         if self.have_position():
-            # 价格绝对止损
-            logger.debug(f"判断平仓参数, finish:{self.market_data.finish == 1}, close:{k.close}, stop_loss_price:{self.g.stop_loss_price}"
-                        f"pdi:{k.up_di} {self.g.pre_up_di}, mdi:{k.down_di} {self.g.pre_down_di}, adx:{k.adx} {self.g.pre_adx}")
             # 退出条件
             if self.is_long_position():
                 if k.up_di < k.down_di and k.adx > self.adx_threshold and k.adx > self.g.pre_adx: # 趋势反转
@@ -105,28 +104,5 @@ class DMIStrategy(DynamicRiskControl, JustFinishKData, BaseStrategy):
                 self.create_order(PositionSide.SHORT, self.max_single_position)
 
 
-
-"""
-交叉
-DI交叉是DMI产生的重要交易信号。 每种交叉都有一组特定的条件。
-
-看涨的DI金叉
-ADX必须超过25（强劲趋势。该值由交易员确定）
-+DI向上穿越-DI。
-止损应设在当天的低点。 如果未突破低点，即使-DI上穿+DI，也不应离场。
-如果ADX上升，则信号增强。
-如果ADX继续增强，交易者应该上调止损。
-
-看空的DI死叉
-ADX必须超过25（强劲趋势。该值由交易员确定）
--DI向上穿越+DI。
-止损应设在当天的高点。 如果高点没有被突破，即使+DI上穿-DI，也不应离场
-如果ADX上升，则信号增强。
-如果ADX继续增强，交易者应该下调止损。
-
-趋向指标（DMI）是Wilder提供的另一个非常有价值的技术分析指标。它以非常复杂的趋势强度和方向为主，并将其计算转化为非常简单明了的视觉效果。
-使用DMI的关键之处在于，即使DMI可以提供有效信息甚至交易信号，但也不容易掌握。为了真正充分利用DMI，技术分析员需要不断研究和调整他们对指标的使用技巧。
-将DMI的基本原理与大量的历史分析、经验相结合，将有助于交易员准确使用DMI，作为其整体交易策略的良好补充。
-"""
 if __name__ == '__main__':
     pass

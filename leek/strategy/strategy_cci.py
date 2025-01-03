5bfc616592d47fd1b203835ff317e8f7f033329d
lishenglin <shenglin.li@metalpha.finance>
2025-01-03 18:31:35 +0800

    feat(code): cci2

--- leek/strategy/strategy_cci.py
+++ leek/strategy/strategy_cci.py
@@ -13,14 +13,14 @@ from leek.strategy.base import Position
 from leek.strategy.common import PositionDirectionManager
 from leek.strategy.common.strategy_common import PositionRateManager
 from leek.strategy.common.strategy_filter import DynamicRiskControl, JustFinishKData
-from leek.t import MA, CCI, CCIV2
+from leek.t import MA, CCI, CCIV2, MACD
 from leek.trade.trade import PositionSide
 
 
 class CCIStrategy(PositionDirectionManager, PositionRateManager, DynamicRiskControl, JustFinishKData, BaseStrategy):
     verbose_name = "CCI简单应用"
 
-    def __init__(self, window=12, fast_period=5, slow_period=20, over_sell=-100, over_buy=100):
+    def __init__(self, window=20, fast_period=12, slow_period=26, over_sell=-100, over_buy=100):
 
         self.fast_ma = MA(int(fast_period))
         self.slow_ma = MA(int(slow_period))
@@ -70,6 +70,63 @@ class CCIStrategy(PositionDirectionManager, PositionRateManager, DynamicRiskCont
             if k.close < k.fast_ma < k.slow_ma and self.can_short():  # 空
                 if k.cci < self.over_sell < k.pre_cci:
                     self.create_order(PositionSide.SHORT, self.max_single_position)
+
+
+class CCIV2Strategy(PositionDirectionManager, PositionRateManager, DynamicRiskControl, JustFinishKData, BaseStrategy):
+    verbose_name = "CCI简单应用2"
+
+    def __init__(self, window=20, fast_period=12, slow_period=26, moving_period=9, over_sell=-100, over_buy=100):
+
+        self.macd = MACD(int(fast_period), int(slow_period), int(moving_period))
+        self.cci = CCI(int(window))
+        # self.cci = CCIV2(int(window))
+
+        self.over_sell = int(over_sell)
+        self.over_buy = int(over_buy)
+
+    def data_init_params(self, market_data):
+        return {
+            "symbol": market_data.symbol,
+            "interval": market_data.interval,
+            "size": 50
+        }
+
+    def _data_init(self, market_datas: list):
+        for market_data in market_datas:
+            self._calculate(market_data)
+        logger.info(f"CCI简单应用数据初始化完成")
+
+    def _calculate(self, data):
+        data.dif, data.dea = self.macd.update(data)
+        last = self.cci.last(1)
+        if len(last) > 0:
+            data.pre_cci = last[-1]
+        data.cci = self.cci.update(data)
+        if data.dif is None or data.dea is None:
+            return
+        data.m = data.dif - data.dea
+
+    def handle(self):
+        k = self.market_data
+        self._calculate(k)
+        if k.m is None or k.cci is None or k.pre_cci is None:
+            return
+        logger.debug(f"CCI指标 close:{k.close} dif:{k.dif} dea:{k.dea} m:{k.m} cci:{k.cci} pre_cci:{k.pre_cci}")
+        if self.have_position():
+            if self.is_long_position():  # 多
+                if k.m < 0 or k.cci < 0:
+                    self.close_position()
+            else:
+                if k.m > 0 or k.cci > 0:
+                    self.close_position()
+        else:
+            if k.m > 0 and self.can_long():  # 多
+                if k.cci > self.over_buy > k.pre_cci:
+                    self.create_order(PositionSide.LONG, self.max_single_position)
+            if k.m < 0 and self.can_short():  # 空
+                if k.cci < self.over_sell < k.pre_cci:
+                    self.create_order(PositionSide.SHORT, self.max_single_position)
+
 
 if __name__ == '__main__':
     pass
--- leek/tests/strategy/strategy_cci_test.py
+++ leek/tests/strategy/strategy_cci_test.py
@@ -13,7 +13,7 @@ import plotly.graph_objs as go
 from leek.runner.view import ViewWorkflow
 from leek.strategy.common.strategy_common import PositionRateManager, PositionDirectionManager
 from leek.strategy.common.strategy_filter import DynamicRiskControl, JustFinishKData
-from leek.strategy.strategy_cci import CCIStrategy
+from leek.strategy.strategy_cci import CCIStrategy, CCIV2Strategy
 from leek.strategy.strategy_dmi import DMIStrategy
 
 
@@ -37,6 +37,29 @@ class TestCCI(unittest.TestCase):
         fig.add_trace(go.Scatter(x=df['Datetime'], y=[100] * len(df["Datetime"]), mode='lines', name='adx_threshold'), row=2, col=1)
         fig.add_trace(go.Scatter(x=df['Datetime'], y=df['cci'], mode='lines', line=dict(color='red', width=1), name='CCI'), row=2, col=1)
         fig.add_trace(go.Scatter(x=df['Datetime'], y=[-100] * len(df["Datetime"]), mode='lines', name='adx_threshold'), row=2, col=1)
+
+        fig.update_xaxes(rangeslider_visible=False, row=1, col=1)
+        fig.show()
+
+    def test_handle2(self):
+        self.strategy = CCIV2Strategy()
+        # workflow = ViewWorkflow(self.strategy, "5m", "2024-12-21 00:00", "2024-12-25 19:30", "CRV-USDT-SWAP")
+        workflow = ViewWorkflow(self.strategy, "5m", "2024-12-01 00:00", "2024-12-20 10:30", "CRV-USDT-SWAP")
+        PositionRateManager.__init__(self.strategy, "1")
+        PositionDirectionManager.__init__(self.strategy, 4)
+        DynamicRiskControl.__init__(self.strategy, window=14, atr_coefficient="2.5", stop_loss_rate="0.02")
+        JustFinishKData.__init__(self.strategy, True)
+        workflow.start()
+        df = pd.DataFrame([x.__json__() for x in workflow.kline_data_g])
+        df['Datetime'] = pd.to_datetime(df['timestamp'] + 8 * 60 * 60 * 1000, unit='ms')
+        fig = make_subplots(rows=3, cols=1, shared_xaxes=True)
+        workflow.draw(fig=fig, df=df)
+
+        fig.add_trace(go.Scatter(x=df['Datetime'], y=[0] * len(df["Datetime"]), mode='lines', name='adx_threshold'), row=2, col=1)
+        fig.add_trace(go.Scatter(x=df['Datetime'], y=df['cci'], mode='lines', line=dict(color='red', width=1), name='CCI'), row=2, col=1)
+
+        fig.add_trace(go.Scatter(x=df['Datetime'], y=df['dif'], mode='lines', name='dif'), row=3, col=1)
+        fig.add_trace(go.Scatter(x=df['Datetime'], y=df['dea'], mode='lines', name='dea'), row=3, col=1)
 
         fig.update_xaxes(rangeslider_visible=False, row=1, col=1)
         fig.show()

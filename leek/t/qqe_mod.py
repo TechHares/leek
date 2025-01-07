2ff953b8ad6e9ada19caa449637b6c2b5661d0f6
Gang Cai <caigang78@gmail.com>
2025-01-08 01:01:36 +0800

    update the qqe mod code

--- leek/t/qqe_mod.py
+++ leek/t/qqe_mod.py
@@ -10,6 +10,18 @@ from leek.t.t import T
 
 
 class QQE:
+    """
+       QQE (Quantitative Qualitative Estimation) 指标类，用于存储QQE指标的结果。
+       QQE MOD指标在设计中综合了多种市场因素，如价格波动、趋势强度等，为投资者提供了全面的市场分析视角。
+       一、红绿灰柱的解读
+        绿柱：当QQE MOD指标显示绿柱时，这通常意味着市场处于上涨趋势中，或者上涨动能正在增强。绿柱的高度和持续时间可以反映上涨趋势的力度和可持续性。在强势市场中，绿柱可能会连续出现，并伴随着股价的不断上涨。
+        红柱：与绿柱相反，红柱的出现通常表示市场处于下跌趋势中，或者下跌动能正在增强。红柱的高度和持续时间同样可以反映下跌趋势的力度和可持续性。在弱势市场中，红柱可能会连续出现，并伴随着股价的不断下跌。
+        灰柱：灰柱的出现则可能表示市场处于相对平衡的状态，或者上涨和下跌动能相互抵消。这种状态下，市场的方向可能不太明确，投资者需要更加谨慎地观察和分析。
+        二、使用场景
+        趋势判断：通过观察QQE MOD红绿灰柱的变化，投资者可以更加直观地判断市场的趋势。例如，当红柱连续出现时，可能意味着市场处于上涨趋势中；而当绿柱连续出现时，则可能表示市场处于下跌趋势中。这有助于投资者更好地把握市场的整体方向。
+        交易决策：在交易过程中，QQE MOD红绿灰柱的变化也可以为投资者提供明确的买入和卖出信号。例如，当市场从绿柱转为红柱时，可能是一个买入的时机；而当市场从红柱转为绿柱时，则可能是一个卖出的时机。当然，投资者还需要结合其他技术指标和市场信息来做出最终的交易决策。
+        风险管理：QQE MOD红绿灰柱还可以用于风险管理。例如，当市场出现连续的绿柱时，投资者可能需要更加警惕市场的下跌风险，并考虑适当减少仓位或设置止损点来规避潜在损失。同样地，当市场出现连续的红柱时，投资者也需要关注市场的过热风险，并考虑适当降低仓位或锁定利润。
+    """
     def __init__(self):
         self.qqeLine = 0.0
         self.histo = 0.0
@@ -18,12 +30,28 @@ class QQE:
 
 
 class QQEMod(T):
-
+    """
+       QQE 指标计算类，继承自 T 类。
+    """
     def __init__(self, window=14,
                  period1=6, SF1=5, QQE1=3, threshold1=3,
                  period2=6, SF2=5, QQE2=1.61, threshold2=3,
                  max_cache=100):
-        T.__init__(self, max_cache)
+        """
+            初始化 QQEMod 类。
+
+            :param window: 窗口大小
+            :param period1: 第一个周期
+            :param SF1: 第一个平滑因子
+            :param QQE1: 第一个QQE因子
+            :param threshold1: 第一个阈值
+            :param period2: 第二个周期
+            :param SF2: 第二个平滑因子
+            :param QQE2: 第二个QQE因子
+            :param threshold2: 第二个阈值
+            :param max_cache: 缓存的最大大小
+        """
+        super().__init__(max_cache)
         self.window = window
 
         self.period1 = period1
@@ -53,6 +81,11 @@ class QQEMod(T):
         )
 
     def _initialize_indicators(self, params):
+        """
+            初始化各种技术指标。
+
+            :param params: 包含周期、平滑因子、wild期和窗口大小的参数字典
+        """
         period = params['period']
         SF = params['SF']  # 这个值可能用于后续计算，但不一定直接用于初始化
         wilder_period = params['wilder_period']
@@ -72,22 +105,27 @@ class QQEMod(T):
         setattr(self, atr_attr_name, SPECIAL_ATR(SF))
 
         # 初始化EMA_ATR
-        atr_attr_name = f"ema_atr_rsi{attr_postfix}"
-        setattr(self, atr_attr_name, SPECIAL_EMA(wilder_period))
+        ema_atr_attr_name = f"ema_atr_rsi{attr_postfix}"
+        setattr(self, ema_atr_attr_name, SPECIAL_EMA(wilder_period))
 
         # 初始化EMA_DAR
-        atr_attr_name = f"ema_dar{attr_postfix}"
-        setattr(self, atr_attr_name, SPECIAL_EMA(wilder_period))
+        ema_dar_attr_name = f"ema_dar{attr_postfix}"
+        setattr(self, ema_dar_attr_name, SPECIAL_EMA(wilder_period))
 
         # 初始化SPECIAL_BAND
-        atr_attr_name = f"special_band{attr_postfix}"
-        setattr(self, atr_attr_name, SPECIAL_BAND(window))
+        special_band_attr_name = f"special_band{attr_postfix}"
+        setattr(self, special_band_attr_name, SPECIAL_BAND(window))
 
-        atr_attr_name = f"ema_fastAtrRsiTL{attr_postfix}"
-        setattr(self, atr_attr_name, SPECIAL_EMA(self.CONST50))
+        ema_fastAtrRsiTL_attr_name = f"ema_fastAtrRsiTL{attr_postfix}"
+        setattr(self, ema_fastAtrRsiTL_attr_name, SPECIAL_EMA(self.CONST50))
 
     def update(self, data):
+        """
+            更新 QQE 指标。
 
+            :param data: 输入数据
+            :return: QQE 对象，包含 QQE 线、历史柱、绿色柱和红色柱的状态
+        """
         qqeMode = QQE()
         try:
             rsi = self.rsi1.update(data)
@@ -149,6 +187,12 @@ class QQEMod(T):
                 self.cache.append(qqeMode)
 
     def bollinger_uplower(self, band):
+        """
+        计算布林带的上下轨。
+
+        :param band: 特殊带对象
+        :return: 上轨和下轨的值
+        """
         basis = self.ema_fastAtrRsiTL1.update(band.fastAtrRsiTL - self.CONST50)
 
         lastBand1 = self.special_band1.last(self.CONST50)
--- leek/t/special.py
+++ leek/t/special.py
@@ -1,63 +1,77 @@
-import math
 from collections import deque
 from decimal import Decimal
 
 import numpy as np
-import pandas as pd
 
-from leek.common import G
 from leek.t.t import T
 
 
 class SPECIAL_EMA(T):
     """
-    加权平均
+    加权平均（指数移动平均线）
     """
 
     def __init__(self, window=9, max_cache=100):
-        T.__init__(self, max_cache)
+        super().__init__(max_cache)
         self.window = window
         self.pre_ma = None
-        self.alpha = float(2 / (self.window + 1))
+        self.alpha = 2 / (self.window + 1)
 
-    def update(self, data: float):
-        ma = None
-        try:
-            if self.pre_ma is None:
-                ma = data
-                return ma
-            ma = self.alpha * data + (1 - self.alpha) * self.pre_ma
-            return ma
-        finally:
-            self.pre_ma = ma
-            self.cache.append(ma)
+    def update(self, data: float) -> float:
+        """
+        更新指数移动平均线。
+
+        :param data: 输入数据
+        :return: 更新后的指数移动平均线值
+        """
+        if self.pre_ma is None:
+            self.pre_ma = data
+            self.cache.append(self.pre_ma)
+            return self.pre_ma
+
+        self.pre_ma = self.alpha * data + (1 - self.alpha) * self.pre_ma
+        self.cache.append(self.pre_ma)
+        return self.pre_ma
 
 
 class SPECIAL_ATR(T):
+    """
+    平均真实波幅（Average True Range）
+    """
+
     def __init__(self, window=14, max_cache=100):
-        T.__init__(self, max_cache)
+        super().__init__(max_cache)
         self.window = window
         self.q = deque(maxlen=window - 1)
 
-    def update(self, data: float):
+    def update(self, data: float) -> float:
+        """
+        更新平均真实波幅。
+
+        :param data: 输入数据
+        :return: 更新后的平均真实波幅值
+        """
         tr = 0.0
         atr = 0.0
-        try:
-            ls = list(self.q)
-            if len(ls) > 0:
-                tr = self.last(1)[0] - data
-                atr = (sum([d.tr for d in ls]) + tr) / (len(ls) + 1)
-            return atr
-        finally:
-            self.q.append(G(tr=tr, atr=atr))
-            self.cache.append(atr)
+
+        if self.q:
+            tr = abs(self.last(1)[0] - data)
+            atr = (sum(d.tr for d in self.q) + tr) / (len(self.q) + 1)
+
+        self.q.append(G(tr=tr, atr=atr))
+        self.cache.append(atr)
+        return atr
 
 
 class QQEBand:
+    """
+    QQE 带对象，存储各种带的值
+    """
+
     def __init__(self, newLongBand=0.0, newShortBand=0.0,
-                       longBand=0.0, shortBand=0.0,
-                       rsiMa=0.0, dar=0.0,
-                       fastAtrRsiTL=0.0):
+                 longBand=0.0, shortBand=0.0,
+                 rsiMa=0.0, dar=0.0,
+                 fastAtrRsiTL=0.0):
         self.newLongBand = float(newLongBand)
         self.newShortBand = float(newShortBand)
         self.longBand = float(longBand)
@@ -68,68 +82,64 @@ class QQEBand:
 
 
 class SPECIAL_BAND(T):
+    """
+    特殊带（SPECIAL_BAND）
+    """
+
     def __init__(self, window=14, max_cache=100):
-        T.__init__(self, max_cache)
+        super().__init__(max_cache)
         self.window = window
         self.q = deque(maxlen=window - 1)
 
-    def update(self, rsiMa, dar):
+    def update(self, rsiMa: float, dar: float) -> QQEBand:
+        """
+        更新特殊带。
+
+        :param rsiMa: RSI 移动平均线值
+        :param dar: DAR 值
+        :return: 更新后的 QQEBand 对象
+        """
         band = QQEBand()
 
-        try:
-            if len(self.q) < 3:
-                return band
-
-            newShortBand = rsiMa + dar
-            newLongBand = rsiMa - dar
-            longBand = 0.0
-            shortBand = 0.0
-            lastBand = self.q[-1]
-
-            if lastBand.rsiMa > lastBand.longBand and rsiMa > lastBand.longBand:
-                longBand = max(lastBand.longBand, newLongBand)
-            else:
-                longBand = newLongBand
-
-            if lastBand.rsiMa < lastBand.shortBand and rsiMa < lastBand.shortBand:
-                shortBand = min(lastBand.shortBand, newShortBand)
-            else:
-                shortBand = newShortBand
-
-            lastLongBand = self.q[-1].longBand
-            lastShortBand = self.q[-1].shortBand
-            lastRsiMa = self.q[-1].rsiMa
-
-            longBandMinus2 = self.q[-2].longBand
-            shortBandMinu2 = self.q[-2].shortBand
-
-            crossAbove1 = (longBandMinus2 < lastRsiMa) & (lastLongBand >= rsiMa)
-            crossBelow1 = (longBandMinus2 > lastRsiMa) & (lastLongBand <= rsiMa)
-            crosses1 = crossAbove1 | crossBelow1
-
-            crossAbove2 = (lastRsiMa < shortBandMinu2) & (rsiMa >= lastShortBand)
-            crossBelow2 = (lastRsiMa > shortBandMinu2) & (rsiMa <= lastShortBand)
-            crosses2 = crossAbove2 | crossBelow2
-
-            trend = (
-                1
-                if crosses2
-                else -1
-                if crosses1
-                else 1
-            )
-
-            fastAtrRsiTL = (longBand if trend == 1 else shortBand)
-
-            band.newLongBand = newLongBand
-            band.newShortBand = newShortBand
-            band.longBand = longBand
-            band.shortBand = shortBand
-            band.rsiMa = rsiMa
-            band.dar = dar
-            band.fastAtrRsiTL = fastAtrRsiTL
-            return band
-
-        finally:
+        if len(self.q) < 3:
             self.q.append(band)
             self.cache.append(band)
+            return band
+
+        newShortBand = rsiMa + dar
+        newLongBand = rsiMa - dar
+        lastBand = self.q[-1]
+
+        longBand = max(lastBand.longBand, newLongBand) if (lastBand.rsiMa > lastBand.longBand and rsiMa > lastBand.longBand) else newLongBand
+        shortBand = min(lastBand.shortBand, newShortBand) if (lastBand.rsiMa < lastBand.shortBand and rsiMa < lastBand.shortBand) else newShortBand
+
+        lastLongBand = lastBand.longBand
+        lastShortBand = lastBand.shortBand
+        lastRsiMa = lastBand.rsiMa
+
+        longBandMinus2 = self.q[-2].longBand
+        shortBandMinus2 = self.q[-2].shortBand
+
+        crossAbove1 = (longBandMinus2 < lastRsiMa) and (lastLongBand >= rsiMa)
+        crossBelow1 = (longBandMinus2 > lastRsiMa) and (lastLongBand <= rsiMa)
+        crosses1 = crossAbove1 or crossBelow1
+
+        crossAbove2 = (lastRsiMa < shortBandMinus2) and (rsiMa >= lastShortBand)
+        crossBelow2 = (lastRsiMa > shortBandMinus2) and (rsiMa <= lastShortBand)
+        crosses2 = crossAbove2 or crossBelow2
+
+        trend = 1 if crosses2 else (-1 if crosses1 else 1)
+
+        fastAtrRsiTL = longBand if trend == 1 else shortBand
+
+        band.newLongBand = newLongBand
+        band.newShortBand = newShortBand
+        band.longBand = longBand
+        band.shortBand = shortBand
+        band.rsiMa = rsiMa
+        band.dar = dar
+        band.fastAtrRsiTL = fastAtrRsiTL
+
+        self.q.append(band)
+        self.cache.append(band)
+        return band
